e51fd7c2cbac48da5e208adaf74de44f4f0d626c
dhruvd2812 <dhruvd2812@gmail.com>
2026-07-23 13:45:51 +0530

    dep2

--- app/Models/department.php
+++ app/Models/department.php
@@ -6,9 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Department extends Model
-{
-    use HasFactory;
+     */
 
     /**
      * Database Table
